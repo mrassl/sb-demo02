095cbcc8e5235286c4f2c053ccb576f62a75139c
Milagros Rassl <milagros.rassl@gmail.com>
2020-04-09 19:18:35 -0400

    commit inicial

---
+++ src/test/java/py/edu/mrassl/sbdemo02/DemoApplication02Tests.java
@@ -0,0 +1,13 @@
+package py.edu.mrassl.sbdemo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplication02Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
